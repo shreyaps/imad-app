b5457dc9bf93c1c919a9eb2c37c8a1e400f5eab4
shreyaps <30948390+shreyaps@users.noreply.github.com>
2017-08-15 18:24:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,8 +34,8 @@ submit.onclick = function(){
     //capture the list of names and render it to a list
     var names = ['name1', 'name2', 'name3', 'name4'] ;
     var list = '';
-    for(var i = 0 ; i<names.length; i++){
-      list = list +  '<li>' + names[i] + '</li>';
+    for(var i = 0 ; i< names.length; i++){
+      list = list += '<li>' + names[i] + '</li>';
         
     }
     var ul = document.getElementById('namelist');
