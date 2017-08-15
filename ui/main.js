d649cbe71aa37592a2156e431a0cb178edca745b
shreyaps <30948390+shreyaps@users.noreply.github.com>
2017-08-15 19:26:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,8 +26,6 @@ request.send(null);
 
 //submit name
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     //Make a request to the server and send the name
@@ -42,3 +40,6 @@ submit.onclick = function(){
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
 };
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
