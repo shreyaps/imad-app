ecfdf6b6d00e8c03b6cb989cdb6a14a547a4f5f8
shreyaps <30948390+shreyaps@users.noreply.github.com>
2017-08-15 11:34:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,3 +22,22 @@ button.onclick = function(){
 request.open('GET', 'http://shreya3112.imad.hasura-app.io/counter', true);
 request.send(null);
 };
+
+
+//submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+    //Make a request to the server and send the name
+    
+    //capture the list of names and render it to a list
+    var names = ['name1', 'name2', 'name3', 'name4'] ;
+    var list = '';
+    for(var i = 0 ; i<names.length; i++){
+      list = list +  '<li>' + names[i] + '</li>';
+        
+    }
+    var ul = document.getElementById('namelist');
+    ul.InnerHTML = list;
+};
