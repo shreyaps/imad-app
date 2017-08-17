475d06f565ec943e4d270b2136d898abb9bf7ed9
shreyaps <30948390+shreyaps@users.noreply.github.com>
2017-08-17 15:49:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,7 +96,6 @@ app.get('/submit-name/:name', function(req, res){//URL: /submit-name?name=xxxx
 });
 
 
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
