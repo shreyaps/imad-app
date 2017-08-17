ff1e156273782b47e66a152a993ff4922771e682
shreyaps <30948390+shreyaps@users.noreply.github.com>
2017-08-17 19:58:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -54,7 +54,7 @@ submit.onclick = function(){
 //Make request
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
-request.open('GET', 'http://shreya3112.imad.hasura-app.io/submit-name?name' + name, true);
+request.open('GET', 'http://shreya3112.imad.hasura-app.io/submit-name?name=' + name, true);
 request.send(null);
     
 };
