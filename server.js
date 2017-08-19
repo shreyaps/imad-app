4be0f2791d9ae74fe032576322a6e4c7764972ca
shreyaps <30948390+shreyaps@users.noreply.github.com>
2017-08-19 13:30:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -118,9 +118,23 @@ app.get('/submit-name', function(req, res){//URL: /submit-name?name=xxxx
    res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName', function(req, res){
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+app.get('/articles/:articleName', function(req, res){
+    //articleName = article-one
+    
+    pool.query("SELECT * FROM article WHERE title =" + req.params.articleName, function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            if(result.rows.length === 0){
+                res.status(400).send("Article not found");
+            }
+            else{
+            var articleData = result.rows[0];
+            res.send(createTemplate(articleData));
+            }
+        }
+    });
 });
 
 app.get('/ui/style.css', function (req, res) {
