42c0d9701f4cfe8031d5fab067612574d6ee8c91
shreyaps <30948390+shreyaps@users.noreply.github.com>
2017-08-20 20:38:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.post('/login', function(req, res){
     var password = req.body.password;
    //take user name and password and enter in DB
    var dbString = hash(password, salt);
-   pool.query('SELECT * FROM "user" username = $1', [username], function(err, result){
+   pool.query('SELECT * FROM "user" WHERE username = $1', [username], function(err, result){
        if(err){
         res.status(500).send(err.toString());
         } else {
