ff9d0be7b086d70094a06a87fd42e1a1d35cfc5e
shreyaps <30948390+shreyaps@users.noreply.github.com>
2017-08-21 11:54:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,30 +1,4 @@
-var button = document.getElementById('counter');
-
-button.onclick = function(){
-    
-    //Create a request object
-    var request = new XMLHttpRequest();
-    
-    //capture the respose and store it in a variable
-    request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-        //take some action
-            if(request.status === 200){
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-    //Not done yet
-    
-    } ;
-//Make request
-request.open('GET', 'http://shreya3112.imad.hasura-app.io/counter', true);
-request.send(null);
-};
-
-
-//submit name
+//submit username and password
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     
@@ -37,24 +11,34 @@ submit.onclick = function(){
         //take some action
             if(request.status === 200){
                 //capture the list of names and render it to a list
-                var names = request.responseText;
+               /* var names = request.responseText;
                 names =JSON.parse(names);
                 var list = '';
                 for(var i = 0 ; i< names.length; i++){
-                   list += '<li>' + names[i] + '</li>';
+                   list += '<li>' + names[i] + '</li>';*/
+                   
+                   console.log("User Successfully logged in");
+                   alert("logged in successfully");
                     
+            }else if(request.status === 403){
+                alert("username pasword are invalid");
+                
+            }else(request.status === 500){
+                alert("something went wrong at the server");
             }
-        var ul = document.getElementById('namelist');
-        ul.innerHTML = list;
+            
         }
     }
     //Not done yet
     
 } ;
+
 //Make request
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-request.open('GET', 'http://shreya3112.imad.hasura-app.io/submit-name?name=' + name, true);
-request.send(null);
-    
+var username = document.getElementById('username').value;
+var password = document.getElementById('password').value;
+console.log(username);
+console.log(password);
+request.open('POST', 'http://shreya3112.imad.hasura-app.io/login' , true);
+request.setRequestHeader('Content-Type', 'application/json');
+request.send(JSON.stringify({username: username, password: password}));
 };
