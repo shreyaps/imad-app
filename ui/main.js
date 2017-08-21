356f720c5eac13f7466d66234bbffb82856d6446
shreyaps <30948390+shreyaps@users.noreply.github.com>
2017-08-21 16:21:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,16 +2,6 @@
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     
-    
-var username = document.getElementById('username').value;
-var password = document.getElementById('password').value;
-console.log(username);
-console.log(password);
-request.open('POST', 'http://shreya3112.imad.hasura-app.io/login' , true);
-request.setRequestHeader('Content-Type', 'application/json');
-request.send(JSON.stringify({username: username, password: password}));
-
-
     //Create a request object
     var request = new XMLHttpRequest();
     
@@ -35,4 +25,12 @@ request.send(JSON.stringify({username: username, password: password}));
     //Not done yet
 
 //Make request
+var username = document.getElementById('username').value;
+var password = document.getElementById('password').value;
+console.log(username);
+console.log(password);
+request.open('POST', 'http://shreya3112.imad.hasura-app.io/login' , true);
+request.setRequestHeader('Content-Type', 'application/json');
+request.send(JSON.stringify({username: username, password: password}));
+
 } ;
