e15b4ec6524d8b40a314a0ab5947d0f60ac934f4
shreyaps <30948390+shreyaps@users.noreply.github.com>
2017-08-21 12:09:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,4 +35,3 @@ console.log(password);
 request.open('POST', 'http://shreya3112.imad.hasura-app.io/login' , true);
 request.setRequestHeader('Content-Type', 'application/json');
 request.send(JSON.stringify({username: username, password: password}));
-};
