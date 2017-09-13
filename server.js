dd063126647901169349a3ae9bc0874c7829fbc0
shreyaps <30948390+shreyaps@users.noreply.github.com>
2017-09-13 09:11:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -158,7 +158,7 @@ app.get('/submit-name', function(req, res){//URL: /submit-name?name=xxxx
    res.send(JSON.stringify(names));
 });
 
-var comments = [];
+var comment = [];
 app.get('/submit-comment', function(req, res){
    
    //Get the comment from the request object
